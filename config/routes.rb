5faa1d2bc6552d5acef339c0b84d1ce557bb009e
Hirohisa Koyama <n88.koyama.hirohisa@gmail.com>
2020-12-20 00:33:21 +0900

    追加: 名前で検索が出来るように実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "pokemons/search",     to: "pokemons#search"
   get "pokemons/get",        to: "pokemons#get"  
   get "pokemons/favorite",   to: "pokemons#favorite"
+  get "pokemons/name",       to: "pokemons#name"
   resources :pokemons, only: [:create, :index, :show] do
     resources :favorites, only: [:create, :destroy]
   end
