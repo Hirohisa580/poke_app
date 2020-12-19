95af802c4bf4302a7daf1659dbb9c11c245b116a
Hirohisa Koyama <n88.koyama.hirohisa@gmail.com>
2020-12-19 16:07:22 +0900

    追加: コントローラー、ビューの記述、ダミーデータ（後に削除）とダミー画像の追加

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,9 +1,37 @@
 class PokemonsController < ApplicationController
 
 def search
+  @pokemon_all = Pokemon.all
+  pokemon_last = Pokemon.last
+  @pokemon_last_id = pokemon_last.number
 end
 
 def get
+  response = Faraday.get "https://pokeapi.co/api/v2/pokemon-species/#{params[:search]}"
+  if response.status == 200
+    @response_body = JSON.parse(response.body)
+    response_names = @response_body["names"]
+    @response = response_names[0]
+    response_name = @response_body["name"]
+
+    response_two = Faraday.get "https://pokeapi.co/api/v2/pokemon/#{response_name}"
+    if response_two.status == 200
+      @response_two = JSON.parse(response_two.body)
+    end
+  end
 end
+
+def create
+  @pokemon = Pokemon.new(pokemon_params)
+  @pokemon.save
+  redirect_to root_path
+end
+
+private
+
+def pokemon_params
+  params.permit(:number, :name, :type_one, :type_two, :image_url)
+end
+
 
 end
